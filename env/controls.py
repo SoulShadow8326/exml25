ab7b1b62c873c880cf4153a616d4d0c6928d99b7
Hursh Bajaj <hurshbajaj2011@gmail.com>
2025-11-13 22:48:37 +0530

    new race_track file

--- env/controls.py
+++ env/controls.py
@@ -1,12 +1,9 @@
 def forward(car):
     car.accelerate_fwd()
-
 def back(car):
     car.accelerate_bck()
-
 def steer_right(car):
     car.steer_right()
-
 def steer_left(car):
     car.steer_left()
 def brake(car):
